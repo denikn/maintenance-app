8704ddc0fdac9eaa680c4f32587ac623445abe57
Jen Jones Arnesen <jarnesen@baosystems.com>
2017-10-17 09:44:21 +0200

    undo linting

--- src/config/maintenance-models.js
+++ src/config/maintenance-models.js
@@ -236,7 +236,7 @@ export function getFiltersForType(modelType) {
 export function getTableColumnsForType(modelType, preservePropNames = false) {
     if (typeDetails.hasOwnProperty(modelType) && typeDetails[modelType].hasOwnProperty('columns')) {
         return typeDetails[modelType].columns
-            .map((col) => preservePropNames ? col : col.replace(/(\w*)\[(\w*)]/, '$1___$2'));
+            .map(col => preservePropNames ? col : col.replace(/(\w*)\[(\w*)]/, '$1___$2'));
     }
 
     // Default columns:
